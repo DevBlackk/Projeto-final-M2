fc4a854b8552b3375ff373f11d709260210b3ba0
Raiane Silva <raianesirlene9@gmail.com>
2023-10-06 19:32:45 -0300

    textos adicionados

---
+++ src/web/pages/creators/script.js
@@ -0,0 +1,7 @@
+function toggleCard() {
+    const card1 = document.querySelector('.card');
+    const card2 = document.querySelector('.card-hidden');
+
+    card1.style.display = 'none';
+    card2.style.display = 'block';
+}
