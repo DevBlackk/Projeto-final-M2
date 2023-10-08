32b9664fcedcad89a8d8d1a86bfe6a75bdfb741f
Daniel <116726675+zedaesquina1@users.noreply.github.com>
2023-10-08 19:40:47 -0300

    quase finalizando o contato

---
+++ src/web/pages/contact/contato.js
@@ -0,0 +1,38 @@
+class ContactForm {
+    constructor() {
+        this.form = document.getElementById('contact-form');
+        this.inputs = this.form.querySelectorAll('.input');
+        this.message = document.getElementById('message');
+        this.form.addEventListener('submit', this.onSubmit.bind(this));
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+
+        let valid = true;
+
+        this.inputs.forEach(input => {
+            if (input.value.trim() === '') {
+                input.style.border = '2px solid red';
+                valid = false;
+            } else {
+                input.style.border = '1px solid black';
+            }
+        });
+
+        if (this.message.value.trim() === '') {
+            this.message.style.border = '2px solid red';
+            valid = false;
+        } else {
+            this.message.style.border = '1px solid black';
+        }
+
+        if (valid) {
+            // Aqui você pode adicionar lógica para enviar o formulário
+            alert('Formulário enviado com sucesso!');
+            this.form.reset();
+        } else {
+            alert('Por favor, preencha todos os campos obrigatórios.');
+        }
+    }
+}
